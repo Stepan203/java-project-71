133e098b4613c77153b8c4aea3f050a7340dcdf0
step203 <stet169500@gmail.com>
2023-03-31 16:41:35 +0300

    Proj_2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,8 @@
 package hexlet.code;
 
-public class Main {
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World!");
     }
 }
+
