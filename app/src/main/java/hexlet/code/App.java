91b2d0cbf90c33c5a7d1cb17b09ca2beab01c289
step203 <stet169500@gmail.com>
2023-04-07 07:53:27 +0300

    checkstyle_Action

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,34 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+     @CommandLine.Parameters(index = "0", paramLabel = "<filepath1>", description = "path to first file")
+    File filepath1;
+    @CommandLine.Parameters(index = "1", paramLabel = "<filepath2>", description = "path to second file")
+    File filepath2;
+
+    @CommandLine.Option(names = {"-f", "--format"}, paramLabel = "format",
+            description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    public static void main(String[] args) throws IOException {
+        int exitCode = new CommandLine(new App()).execute(args);
+        Differ.generate();
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        //String result = Differ.generate(filepath1, filepath2);
+     //   System.out.println(result);
+        System.out.println("Hello");
+        return 0;
     }
 }
-
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,40 @@
+package hexlet.code;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.*;
+
+public class Differ {
+
+    public static void generate() throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> file1 = objectMapper.readValue(new File("file1.json"), Map.class);
+        Map<String, Object> file2 = objectMapper.readValue(new File("file2.json"), Map.class);
+        for (Map.Entry<String, Object> pair : file1.entrySet()) {
+            if (!file2.containsKey(pair.getKey())) {
+                System.out.println("- " + pair.getKey() + ": " + pair.getValue());
+            }
+            if (file2.containsKey(pair.getKey()) && file2.containsValue(pair.getValue())) {
+                System.out.println(pair.getKey() + ": " + pair.getValue());
+            }
+        }
+            for(Map.Entry<String, Object> pair2: file2.entrySet()) {
+                if (!file1.containsKey(pair2.getKey())) {
+                    System.out.println("+ " + pair2.getKey() + ": " + pair2.getValue());
+                }
+                if (file1.containsKey(pair2.getKey()) && !file1.containsValue(pair2.getValue())) {
+                    System.out.println("- " + pair2.getKey() + ": " + file1.get(pair2.getKey()));
+                    System.out.println("+ " + pair2.getKey() + ": " + pair2.getValue());
+                }
+            }
+
+           // System.out.println(String.format("Key (name) is: %s, Value (age) is : %s", pair.getKey(), pair.getValue()));
+      //  }
+      //  System.out.println(file1);
+      //  System.out.println(file2);
+    }
+}
