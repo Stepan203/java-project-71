831c4834218fa221671cab4afb2006e6383dc975
step203 <stet169500@gmail.com>
2023-04-09 17:21:38 +0300

    Repo

---
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -0,0 +1,24 @@
+package hexlet.code;
+
+//package io.hexlet.boilerplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DifferTest {
+    @Test
+    public void test1() {
+        var 
+        assertEquals(Differ.generate(), Differ.generate);
+
+    }
+
+}
