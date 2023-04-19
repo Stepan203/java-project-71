dcb39dd7e54c7be35f90916f405553fae4a7a74a
step203 <stet169500@gmail.com>
2023-04-19 18:45:22 +0300

    SB

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,38 +6,41 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.*;
 
 public class Differ {
-    public static LinkedHashMap generate(String file_1, String file_2) throws IOException {
+    public static String generate(String file_1, String file_2) throws IOException {
 
         ObjectMapper objectMapper = new ObjectMapper();
         Map<String, Object> file1 = objectMapper.readValue(new File("file1.json"), Map.class);
         Map<String, Object> file2 = objectMapper.readValue(new File("file2.json"), Map.class);
         HashSet<String> countryHashSet = new HashSet<>();
-        LinkedHashMap lhm = new LinkedHashMap();
-        //ArrayList<String> myArrayList = new ArrayList<String>();
-        // StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String, Object> pair : file1.entrySet()) {
-            countryHashSet.add(pair.getKey());
-            }
+        Map<String, Object> lhm = new TreeMap<String, Object>();
+        StringBuilder sb = new StringBuilder("{ \n");
+        for (Map.Entry<String, Object> pair1 : file1.entrySet()) {
+            countryHashSet.add(pair1.getKey());
+        }
         for (Map.Entry<String, Object> pair2 : file2.entrySet()) {
             countryHashSet.add(pair2.getKey());
-            }
+        }
         countryHashSet.stream().forEach((keySet) -> {
-            if (file1.containsKey(keySet) && !file2.containsKey(keySet)) {
-                lhm.put(file1.get(keySet), " - " + keySet);
-            }
-            if (file1.containsKey(keySet) && file2.containsKey(keySet)) {
-                if (file1.get(keySet).equals(file2.get(keySet))) {
-                    lhm.put(file1.get(keySet), keySet);
-                }
-                else
-                lhm.put(file1.get(keySet), keySet);
-                lhm.put(file2.get(keySet), keySet);
-            }
-            if (!file1.containsKey(keySet) && file2.containsKey(keySet)) {
-                lhm.put(file2.get(keySet), " + " + keySet);
-            }
+            lhm.put(keySet, file1.get(keySet));
+            lhm.put(keySet, file1.get(keySet));
         });
-        lhm.forEach((key, value) -> System.out.printf((String) value, key));
-        return lhm;
+        for (String key : lhm.keySet()) {
+            if (file1.containsKey(key) && !file2.containsKey(key)) {
+                sb.append(" - " + key + ": " + lhm.get(key) + "\n");
+            }
+            if (file1.containsKey(key) && file2.containsKey(key)) {
+                if (file1.get(key).equals(file2.get(key)))
+                    sb.append(" " + key + ": " + lhm.get(key) + "\n");
+            }
+            if (file1.containsKey(key) && file2.containsKey(key) && !file1.get(key).equals(file2.get(key))) {
+                sb.append(" - " + key + ": " + file1.get(key) + "\n");
+                sb.append(" + " + key + ": " + file2.get(key) + "\n");
+        }
+        if (!file1.containsKey(key) && file2.containsKey(key)) {
+            sb.append(" + " + key + ": " + file2.get(key) + "\n");
+        }
+    }
+        sb.append("}");
+        return sb.toString();
     }
 }
