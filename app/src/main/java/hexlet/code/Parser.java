6b4418e1a4eac160f775c4e16ce828e4f8f87bb6
step203 <stet169500@gmail.com>
2023-06-01 07:09:02 +0300

    parser

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,19 +1,7 @@
 package hexlet.code;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.lang.runtime.ObjectMethods;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.TreeMap;
-
-import org.yaml.snakeyaml.Yaml;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
@@ -22,7 +10,6 @@ public class Parser {
 
         Map<String, Object> map = new HashMap<>();
         ObjectMapper mapper;
-        //ObjectMapper om = new ObjectMapper();
         if (StringDir.contains(".json")) {
             mapper = new ObjectMapper();
             map = mapper.readValue(content, Map.class);
