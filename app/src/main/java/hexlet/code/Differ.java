b067cecf07c73d5dcc06fe689e884b0dee7590b4
step203 <stet169500@gmail.com>
2023-06-06 08:05:27 +0300

    Dif

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -22,7 +22,6 @@ public class Differ {
         //for(int i = 0; i < subStr.length; i++) {
       //  System.out.println("Str " + format);
        // }
-
         Map<String, Object> data1 = Parser.typeParser(file1, content1);
         Map<String, Object> data2 = Parser.typeParser(file2, content2);
 
